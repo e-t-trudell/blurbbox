9cd1af3a776e6bf77137d2e3940b4c31102cad1c
Christian Lozano <christianlozano729@gmail.com>
2022-11-30 12:07:21 -0600

    cafe business logic

---
+++ Fundamentals/CafeBusinessLogic/CafeUtil.java
@@ -0,0 +1,9 @@
+// This is the file for all my methods
+class CafeUtil {
+
+    public int getStreakGoal(int someNumber){
+        // Here we will write out the logic we need in order to 
+        //implement the reward system
+        return "";
+    }
+}
---
+++ Fundamentals/CafeBusinessLogic/TestCafe.java
@@ -0,0 +1,12 @@
+// This is the test file
+import java.util.ArrayList;
+import java.util.Arrays;
+public class TestCafe {
+    public static void main(String[] args) {
+
+        CafeUtil otherfile = new CafeUtil();
+
+        System.out.println("\n------ Streak Goal Test -----");
+        System.out.println("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
+    }
+}

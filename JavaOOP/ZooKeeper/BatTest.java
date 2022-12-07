c822c2bac563451bb2bbfd2428fe442f8e498e0e
Christian Lozano <christianlozano729@gmail.com>
2022-12-07 10:04:53 -0600

    ZooKeeper commit

---
+++ JavaOOP/ZooKeeper/BatTest.java
@@ -0,0 +1,12 @@
+public class BatTest{
+    public static void main(String[] args) {
+        Bat bat = new Bat();// why do we need to input a value on gorilla but not bat?
+        bat.attackTown();
+        bat.attackTown();
+        bat.attackTown();
+        bat.eatHumans();
+        bat.eatHumans();
+        bat.fly();
+        bat.fly();
+    }
+}
---
+++ JavaOOP/ZooKeeper/GorillaTest.java
@@ -0,0 +1,11 @@
+public class GorillaTest{
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla(100);
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.eatBananas();
+        gorilla.eatBananas();
+        gorilla.climb();
+    }
+}

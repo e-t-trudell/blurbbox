14f305ec2c2a95490c183ea78951020e986de4ce
Christian Lozano <christianlozano729@gmail.com>
2022-12-10 22:52:13 -0600

    update

--- SpringProjects/DaichiPathVariables/src/main/java/com/codingdojo/daichivariables/DaikichiController.java
+++ SpringProjects/DaichiPathVariables/src/main/java/com/codingdojo/daichivariables/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codingdojo.daichivariables;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DaikichiController {
 		return "Welcome";
 	}
 	
-	@RequestMapping("/today")
+	@RequestMapping("/")
 	public String today() {
 		return "Today you will find luck in all your endeavors!";
 	}
@@ -21,4 +22,19 @@ public class DaikichiController {
 		return "Tomorrow, an oppurtunity will arise, so be sure to open to new ideas!";
 	}
 	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lottery(@PathVariable int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but the weary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
+			
+	}
 }

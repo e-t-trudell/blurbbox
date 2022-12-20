3486e048d5ee79f27bd4918000b21fbaddc82d3b
Christian Lozano <christianlozano729@gmail.com>
2022-12-19 19:24:02 -0600

    rendering all

---
+++ SpringProjects/AllBooks/src/main/java/com/codingdojo/allbooks/AllBooksApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.allbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllBooksApplication.class, args);
+	}
+
+}
--- SpringProjects/AllBooks/src/main/java/com/codingdojo/allbooks/controllers/BooksController.java
+++ SpringProjects/AllBooks/src/main/java/com/codingdojo/allbooks/controllers/BooksController.java
@@ -1,4 +1,6 @@
-package com.codingdojo.renderingbooks.controllers;
+package com.codingdojo.allbooks.controllers;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,22 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.codingdojo.renderingbooks.models.Book;
-import com.codingdojo.renderingbooks.services.BookService;
+import com.codingdojo.allbooks.models.Book;
+import com.codingdojo.allbooks.services.BookService;
 
-@Controller 
-public class BookController {
+@Controller
+public class BooksController {
+	
 	
 	@Autowired
 	BookService bookService;
 	
 	
-	
-	@GetMapping("/")
-	public String start() {
-		return "home.jsp";
+	@GetMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		return "all.jsp";
 	}
-	
 	@GetMapping("/books/{id}")
 	public String book(@PathVariable("id") Long id, Model model) {
 		

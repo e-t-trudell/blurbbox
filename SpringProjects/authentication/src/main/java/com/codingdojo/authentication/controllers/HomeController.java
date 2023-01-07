c9c7a8c36a958e845e466ec94a751b318f3dead5
Christian Lozano <christianlozano729@gmail.com>
2023-01-07 11:28:38 -0600

    authen

---
+++ SpringProjects/authentication/src/main/java/com/codingdojo/authentication/controllers/HomeController.java
@@ -0,0 +1,70 @@
+package com.codingdojo.authentication.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.codingdojo.authentication.models.LoginUser;
+import com.codingdojo.authentication.models.User;
+
+public class HomeController {
+	
+	// Add once service is implemented:
+    // @Autowired
+    // private UserService userServ;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+    
+        // Bind empty User and LoginUser objects to the JSP
+        // to capture the form input
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+    
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // TO-DO Later -- call a register method in the service 
+        // to do some extra validations and create a new user!
+        
+        if(result.hasErrors()) {
+            // Be sure to send in the empty LoginUser before 
+            // re-rendering the page.
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+    
+        return "redirect:/home";
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // Add once service is implemented:
+        // User user = userServ.login(newLogin, result);
+    
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+    
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+    
+        return "redirect:/home";
+    }
+}
---
+++ SpringProjects/authentication/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page isErrorPage="true" %>
+<%@ taglib prefix = "form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<!-- for Bootstrap CSS -->
+<link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css" />
+<!-- YOUR own local CSS -->
+<link rel="stylesheet" href="/css/style.css"/>
+<!-- For any Bootstrap that uses JS or jQuery-->
+<script src="/webjars/jquery/jquery.min.js"></script>
+<script src="/webjars/bootstrap/js/bootstrap.min.js"></script>
+<title>Login & Registration</title>
+</head>
+<body class="container">
+	<h1>Welcome!</h1>
+	<p>Join our growing community.</p>
+	<form:form method="post" modelAttribute="newUser" class="form">
+		<div>
+			<h2>Register</h2>
+			<div class="form-group">
+				<form:label path="userName">User Name</form:label>
+				<form:input path="userName" class="form-control"/>
+			</div>
+			<div class="form-group">
+				<form:label path="email">Email:</form:label>
+				<form:input path="email" class="form-control"/>
+			</div>
+			<div class="form-group">
+				<form:label path="password">Password:</form:label>
+				<form:input path="password" class="form-control"/>
+			</div>
+			<div class="form-group">
+				<form:label path="confirm">Confirm PW:</form:label>
+				<form:input path="confirm" class="form-control"/>
+			</div>
+		</div>
+	</form:form>
+	<form:form method="post" modelAttribute="newLogin" class="form">
+		<div>
+			<h2>Log in</h2>
+			<div class="form-group">
+				<form:label path="email">Email:</form:label>
+				<form:input path="email" class="form-control"/>
+			</div>
+			<div class="form-group">
+				<form:label path="password">Password:</form:label>
+				<form:input path="password" class="form-control"/>
+			</div>
+		</div>
+	</form:form>
+	
+</body>
+</html>

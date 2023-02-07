15bf4e5b62b8f40b642ac4ef3610f152dc2eff1e
Christian Lozano <christianlozano729@gmail.com>
2023-02-07 10:54:31 -0600

    Chore tracker

---
+++ SpringProjects/ChoreTracker/src/test/java/com/codingdojo/choretracker/ChoreTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.choretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

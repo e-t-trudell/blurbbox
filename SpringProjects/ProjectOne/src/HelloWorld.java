6c0fdcd4131506f964f02a714abcd79058fcf5dc
Eric Trudell <106626020+e-t-trudell@users.noreply.github.com>
2023-02-26 18:23:01 -0700

    Delete SpringProjects/ProjectOne directory

--- SpringProjects/ProjectOne/src/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-
-public class HelloWorld {
-	public String greet() {
-		return "Hello world!";
-	}
-}
--- SpringProjects/ProjectOne/src/HelloWorldTest.java
+++
@@ -1,9 +0,0 @@
-
-public class HelloWorldTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}

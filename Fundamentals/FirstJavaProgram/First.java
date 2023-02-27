a3298aa1a5007392f1cb6bd05cf67627be0d6dca
Eric Trudell <106626020+e-t-trudell@users.noreply.github.com>
2023-02-26 18:18:31 -0700

    Delete Fundamentals directory

--- Fundamentals/FirstJavaProgram/First.java
+++
@@ -1,8 +0,0 @@
-public class First {
-    public static void main(String[] args){
-
-        System.out.println("My name is christian");
-        System.out.println("Im 25 years old");
-        System.out.println("My hometown is Oklahoma City");
-    }
-}

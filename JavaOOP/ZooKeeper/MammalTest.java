ef2afd561488175b12fb277ad253e89fb16f70e6
Eric Trudell <106626020+e-t-trudell@users.noreply.github.com>
2023-02-26 18:19:04 -0700

    Delete JavaOOP directory

--- JavaOOP/ZooKeeper/MammalTest.java
+++
@@ -1,8 +0,0 @@
-// so in this file we wanna write out our launchpad for the code to run in the terminal
-class MammalTest {
-    public static void main(String[] args) {
-        Mammal gorilla = new Mammal();
-        int energyLevel = gorilla.getEnergyLevel();
-        System.out.println("The mammals energy level is at: " + energyLevel);
-    }
-}
